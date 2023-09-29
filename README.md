b2326d80457d326d515994b84973b71a3b29f49d
FranciscoBS89 <francisco.spadaro@outlook.com>
2023-09-29 15:43:03 -0300

    first Commit
    inicio do projeto

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CodePipeline
  Caso de uso de app React usando AWS CodePipeline, CodeBuild, CodeCommit
+
+ - iniciando projeto
+
+ yarn create react-app sample-repo
+ or
+ npx create-react-app sample-repo
+
+- AWS S3
+proximo passo é criar um bucket s3 na aws onde vai será feito o upload desse projeto e suas atualizações
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Pagina inicial do projeto Aws Code Pipeline .
+        </p>
+        <a
+          className="App-link"
+          href="https://fbs-dev.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FBS-DEV
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
